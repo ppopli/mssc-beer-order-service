afd91a950c975d150039b93a62a989cd9a3e0993
PulkitPopli <ppopli2793@gmail.com>
2023-07-24 02:40:43 +0530

    Add consumer in beer inventory service to handle cancel order request

--- src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -15,7 +15,7 @@ public class JmsConfig {
   public static final String ALLOCATE_ORDER_RESPONSE_QUEUE = "allocate-order-result";
 
   public static final String ALLOCATE_FAILURE_QUEUE = "allocation-failure";
-    public static final String DEALLOCATE_ORDER_QUEUE = "deallocate-order";
+  public static final String DEALLOCATE_ORDER_QUEUE = "deallocate-order";
 
     @Bean
   public MessageConverter messageConverter(ObjectMapper objectMapper) {
